15ccd932bfe796bcdce068dff7b0364d882ae064
abigailforeman <forea005@gmail.com>
2020-04-22 23:46:27 +0100

    nav bar updates and removed some flowers for larger screen

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,14 @@
 function init() {
 
   const navText = document.querySelectorAll('.navtext')
-  const about = document.querySelector('#about')
-  // console.log(about)
-  // const skills = document.querySelector('#skills')
+  const pageSections = [...document.querySelectorAll('.page-section')]
+  console.log(pageSections)
+
+  const navLinks = [...document.querySelectorAll('.navtext')]
+  console.log(navLinks)
+
+
+  // ! Function to add an active class to the nav link when it's clicked
 
   // Loop through and add the active class to the current/clicked link
   for (let i = 0; i < navText.length; i++) {
@@ -22,30 +27,39 @@ function init() {
 
 
 
+  // ! Function to check if a section is in viewport
 
+  // * this function returns a boolean based on whether the section passed to it is in viewport 
 
-  function isInViewport(elem) {
+  function isInViewport (elem) {
     const distance = elem.getBoundingClientRect()
-    // console.log(distance)
-    console.log(elem)
     return (
       distance.top >= 0 &&
-      distance.left >= 0 &&
-      distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+      distance.left >= 0 
+      // distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      // distance.right <= (window.innerWidth || document.documentElement.clientWidth)
     )
   }
 
+  // * this event listener is added to window scrolling and checks if isInViewport function returns true or false 
 
   window.addEventListener('scroll', function () {
-    // console.log('scrolling')
-    // console.log(Boolean(isInViewport(about)))
-    if (isInViewport(about)) {
-      console.log('about is in viewport!')
-    } else {
-      console.log('nope')
-    }
-  }, false)
+    
+    // navLinks.forEach(navLink => navLink.classList.remove('active'))
+
+    pageSections.filter(pageSection => {
+      if (isInViewport(pageSection)) {
+        console.log(isInViewport(pageSection))
+        console.log(pageSection.id, 'NOT in viewport!')
+        navLinks[pageSections.indexOf(pageSection)].classList.remove('active')
+        return
+      } else {
+        console.log(isInViewport(pageSection))
+        console.log(pageSection.id, 'is in viewport')
+        navLinks[pageSections.indexOf(pageSection)].classList.add('active')
+      }
+    })
+  })
 
 
 }
