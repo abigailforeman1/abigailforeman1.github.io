2977e77729796a2063800bb9d5a33003867afe4b
abigailforeman <forea005@gmail.com>
2020-05-12 23:30:02 +0100

    trying to fix scroll nav bar behaviour

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,4 @@
-function init() {
-
+function init () {
   const navText = document.querySelectorAll('.navtext')
   const pageSections = [...document.querySelectorAll('.page-section')]
   const navLinks = [...document.querySelectorAll('.navtext')]
@@ -23,39 +22,53 @@ function init() {
 
   // ! Function to check if a section is in viewport
 
-  // * this function returns a boolean based on whether the section passed to it is in viewport 
+  // * this function returns a boolean based on whether the section passed to it is in viewport
 
   function isInViewport (elem) {
     const distance = elem.getBoundingClientRect()
+
+    // const numbers = []
+    // for (let i = distance.top; i <= distance.bottom; i++) {
+    //   numbers.push(i)
+    // }
+    // console.log(numbers)
+
+    // numbers.some(number => {
+    //   return console.log(number >= 0 && number <= window.innerHeight)
+    // })
+
+    // console.log('distance', elem, distance)
+    // console.log(window.innerHeight)
+    // console.log('distance.bottom', elem, distance.bottom)
+
+    // if numbers is greater or equal to 0 or less than or equal to window.innerHeight
+
     return (
-      distance.top >= 0 &&
-      distance.left >= 0 
-      // distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      // distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+      distance.top >= 0 && distance.top <= window.innerHeight
+    // distance.bottom <= (window.innerHeight || document.documentElement.clientHeight)
     )
   }
 
-  // * this event listener is added to window scrolling and checks if isInViewport function returns true or false 
+  // * this event listener is added to window scrolling and checks if isInViewport function returns true or false
 
   window.addEventListener('scroll', function () {
-    
     // navLinks.forEach(navLink => navLink.classList.remove('active'))
 
     pageSections.filter(pageSection => {
+      // console.log('1', window.innerHeight)
+      // console.log('2', document.documentElement.clientHeight)
       if (isInViewport(pageSection)) {
-        console.log(isInViewport(pageSection))
-        console.log(pageSection.id, 'NOT in viewport!')
-        navLinks[pageSections.indexOf(pageSection)].classList.remove('active')
+        // console.log(isInViewport(pageSection))
+        // console.log(pageSection.id, 'NOT in viewport!')
+        navLinks[pageSections.indexOf(pageSection)].classList.add('active')
         return
       } else {
-        console.log(isInViewport(pageSection))
-        console.log(pageSection.id, 'is in viewport')
-        navLinks[pageSections.indexOf(pageSection)].classList.add('active')
+        // console.log(isInViewport(pageSection))
+        // console.log(pageSection.id, 'is in viewport')
+        navLinks[pageSections.indexOf(pageSection)].classList.remove('active')
       }
     })
   })
-
-
 }
 
 window.addEventListener('DOMContentLoaded', init)
